3b8c48515b2897486966f77e09c07c092cbfb64f
kefi550 <ytiryrstd550@gmail.com>
2022-10-12 15:20:43 +0900

    headlessにすると初回起動に失敗したりしたのでheadlessでやらないようにする

--- moneyforward_selenium/moneyforward_selenium.py
+++ moneyforward_selenium/moneyforward_selenium.py
@@ -18,10 +18,11 @@ SELENIUM_HOST = os.environ.get("SELENIUM_HOST", "127.0.0.1")
 SELENIUM_PORT = os.environ.get("SELENIUM_PORT", "4444")
 
 options = ChromeOptions()
-options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+# docker-seleniumでheadlessすると初回実行で失敗するっぽいのでnot headlessでいく
+# options.add_argument("--headless")
 
 
 def _chrome_profile(filepath: str = "./chrome_profile"):
